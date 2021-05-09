4ee06f6d6844d64574a08acd1e3e3c812b855cf5
cristianeasreis <cristianeasreis@hotmail.com>
2021-05-09 00:10:13 -0300

    estudando read2

--- frontend/src/app/components/product/product-create/product-create.component.ts
+++ frontend/src/app/components/product/product-create/product-create.component.ts
@@ -1,35 +1,36 @@
-import { Product } from "./../product.model";
-import { ProductService } from "./../product.service";
-import { Component, OnInit } from "@angular/core";
-import { Router } from "@angular/router";
-
+import { Product } from './../product.model';
+import { ProductService } from './../product.service';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: "app-product-create",
-  templateUrl: "./product-create.component.html",
-  styleUrls: ["./product-create.component.css"],
+  selector: 'app-product-create',
+  templateUrl: './product-create.component.html',
+  styleUrls: ['./product-create.component.css']
 })
 export class ProductCreateComponent implements OnInit {
 
-  constructor(private ProductService: ProductService, private router: Router) {}
-
   product: Product = {
     name: '',
-    price: '',
+    price: null
+  }
+
+  constructor(private productService: ProductService,
+      private router: Router) { }
+
+  ngOnInit(): void {
     
   }
 
-  ngOnInit(): void {
-
-  }
-
   createProduct(): void {
-    this.ProductService.create(this.product).subscribe(() => {
-      this.ProductService.showOnMessage("Produto Criado!");
-      this.router.navigate(["/products"]);
-    });
+    this.productService.create(this.product).subscribe(() => {
+      this.productService.showMessage('Produto criado!')
+      this.router.navigate(['/products'])
+    })
+
   }
+
   cancel(): void {
-    this.router.navigate(["/products"]);
+    this.router.navigate(['/products'])
   }
 }
--- frontend/src/app/components/product/product-read/product-read.component.ts
+++ frontend/src/app/components/product/product-read/product-read.component.ts
@@ -1,4 +1,3 @@
-  
 import { ProductService } from './../product.service';
 import { Product } from './../product.model';
 import { Component, OnInit } from '@angular/core';
@@ -10,15 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductReadComponent implements OnInit {
 
-  products?: Product[];
- 
+  products: Product[]
+  displayedColumns = ['id', 'name', 'price', 'action']
   
   constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
     this.productService.read().subscribe(products => {
       this.products = products
-      console.log(products)
     })
   }
 
